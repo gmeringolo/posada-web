1f4b454df9243f65fba707de7ea958ed5bc8decf
gianella.meringolo <gmeringolo@gmail.com>
2022-10-13 12:14:17 -0300

    header responsive in screen size 1280px

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Router>
         <Switch>
           <Route path="/" exact component={Home} />
         </Switch>
       </Router>
-    </React.Fragment>
+    </>
   );
 }
 
