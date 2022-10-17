55673192351a509432c5e2d2bd5600922b306f54
gianella.meringolo <gmeringolo@gmail.com>
2022-10-17 11:59:14 -0300

    router dom update last version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter  as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Home from "./pages/Home";
+
 
 
 function App() {
   return (
-    <>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          { /* Es muy recomendable añadir esta ruta para obtener un mensaje de error en el caso de que la ruta no exista. De lo contrario, si la ruta no existe llegaremos a una página en blanco     
+          <Route path="*" element={<div>404</div> } />*/}   
+      </Routes>
+    </Router>
   );
 }
 
