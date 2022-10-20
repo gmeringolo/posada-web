8130f26291b454e7e37843a4da4ed41d875867ea
gianella.meringolo <gmeringolo@gmail.com>
2022-10-20 14:04:09 -0300

    responsive for tablet resolution

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 //css
 import '../assets/css/home.css'
-import Header from '../components/header.js';
+import Header from '../components/header2';
 //import Banner from '../components/banner.js';
 
 function Home() {
     return (
         <>
-        <div className='img'>
           <Header /> 
-          <div class="titular">
-           <h1>Casas totalmente equipadas</h1>
-            <p>Ubicadas en zona muy tranquila y a media cuadra de la playa en balneario Bello horizonte (Canelones)</p>
-          </div>
-        </div>  
-
         </>
       );    
     }
