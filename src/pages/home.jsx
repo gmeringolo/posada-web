1c45279c4aec4b0eeafeb6521f468fc359aac568
gianella.meringolo <gmeringolo@gmail.com>
2022-11-20 12:07:01 -0300

    boton flotante whatsapp en home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "./home.module.css";
 import Header from "../components/header/Header";
 import Casas from "../components/casas/Casas";
 import Servicios from "../components/servicios/Servicios";
 import Tarifas from "../components/tarifas/Tarifas";
 import Contacto from "../components/contacto/Contacto";
+import icono from "./icono.png";
 
 function home() {
   return (
@@ -13,6 +15,11 @@ function home() {
       <Servicios />
       <Tarifas />
       <Contacto />
+      <div className={styles.containerBoton}>
+        <a href="https://wa.me/+59899667726?text=Para%20consultar%20disponibilidad,%20indicar%20fecha%20de%20entrada%20y%20salida" target="_blank" rel="noreferrer">
+            <img class={styles.boton} src={icono} alt=""/>
+        </a>
+    </div>
     </>
   );
 }
