824e1b76c330ef8c2b50e4694a7302c01b8a316f
Gianella Meringolo <83734502+gmeringolo@users.noreply.github.com>
2023-10-11 18:29:16 -0300

    Update Tarifas.jsx

--- src/components/tarifas/Tarifas.jsx
+++ src/components/tarifas/Tarifas.jsx
@@ -12,24 +12,19 @@ function Tarifas() {
     <>
       <div className={styles.textoTarifas} id="tarifas">
         <h1>Tarifas y Promociones</h1>
-        <p>Precios temporada 2023</p>
+        <p>Precios temporada 2023-2024</p>
         <div className={styles.tarifas}>
-          <img src={precios} className={styles.imgTarifas} alt="Tarifas 2023" />
+          <img
+            src={baja}
+            className={styles.imgTarifas}
+            alt="precio temp baja 2023"
+          />
+          <img src={precios} className={styles.imgTarifas} alt="Tarifas 2024" />
           <img src={enero} className={styles.imgTarifas} alt="Promo enero" />
           <img
             src={marzo}
             className={styles.imgTarifas}
             alt="Precio marzo 2023"
-          />
-          <img
-            src={turismo}
-            className={styles.imgTarifas}
-            alt="Precio turismo 2023"
-          />
-          <img
-            src={baja}
-            className={styles.imgTarifas}
-            alt="precio temp baja 2023"
           />
         </div>
       </div>
