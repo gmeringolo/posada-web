871e9a203012074cb577b48e86178461afd37609
Gianella Meringolo <gmeringolo@gmail.com>
2024-10-29 21:32:54 -0300

    actualizacion precios 2025

--- src/components/tarifas/Tarifas.jsx
+++ src/components/tarifas/Tarifas.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./tarifas.module.css";
-import precios from "./tarifas24.jpg";
+import precios from "./tarifas25.png";
 import baja from "./baja.png";
 import marzo from "./marzo.png";
 import primavera from "./promo.jpeg";
@@ -14,22 +14,22 @@ function Tarifas() {
         <p>Precios temporada 2024</p>
         <div className={styles.tarifas}>
 
-          <img src={precios} className={styles.imgTarifas} alt="Tarifas 2024" />
+        <img
+            src={primavera}
+            className={styles.imgTarifas}
+            alt="precio primavera 2024"
+          />  
+          <img src={precios} className={styles.imgTarifas} alt="Tarifas 2025" />
           <img
             src={marzo}
             className={styles.imgTarifas}
-            alt="Precio marzo 2024"
+            alt="Precio marzo 2025"
           />
           <img
             src={baja}
             className={styles.imgTarifas}
             alt="precio temp baja 2024"
-          />
-          <img
-            src={primavera}
-            className={styles.imgTarifas}
-            alt="precio primavera 2024"
-          />      
+          />    
         </div>
       </div>
     </>
